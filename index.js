094dd9c7d07739668eba9aa00bbcac8fb506c7db
CodeMonkey <ilovevar@gmail.com>
2018-03-12 23:07:02 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 var mpg = require('mpg123')
+let request = require('request')
+
+let fs = require('fs')
 
 var player = new mpg.MpgPlayer()
 
 let msg = 'abc'
 
-player.play(`https://ai.baidu.com/aidemo?type=tns2&idx=1&tex=${decodeURIComponent(msg)}&cuid=baidu_speech_demo&cod=2&lan=zh&ctp=1&pdt=1&spd=5&per=4&vol=9&pit=5`);
+const stream = request(`https://ai.baidu.com/aidemo?type=tns2&idx=1&tex=${decodeURIComponent(msg)}&cuid=baidu_speech_demo&cod=2&lan=zh&ctp=1&pdt=1&spd=5&per=4&vol=9&pit=5`)
+    .pipe(fs.createWriteStream('test.mp3'))
+
+stream.on('finish', () => {
+    player.play(`${__dirname}/test.mp3`)
+});
