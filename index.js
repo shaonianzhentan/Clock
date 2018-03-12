6b92b9cd17bf7233c4ee866e66c4c48b3755d15c
CodeMonkey <ilovevar@gmail.com>
2018-03-12 23:36:04 +0800

    Update index.js

--- index.js
+++ index.js
@@ -7,14 +7,7 @@ var player = new mpg.MpgPlayer()
 
 let msg = '现在时间是23:50'
 
-
-stream.on('finish', () => {
-    console.log('play')
+console.log('play')
       var mp3Url = "http://tts.baidu.com/text2audio?idx=1&tex=" + encodeURIComponent(msg) + "&cuid=baidu_speech_demo&cod=2&lan=zh&ctp=1&pdt=1&spd=5&per=4&vol=5&pit=5"
   
     player.play(mp3Url)
-});
-
-stream.on('error', err => {
-    console.log(err)
-});
