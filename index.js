86eef8c2ec6f638f4e129547b8a2feedd8ff3eb3
CodeMonkey <ilovevar@gmail.com>
2018-03-12 23:22:38 +0800

    Update index.js

--- index.js
+++ index.js
@@ -5,11 +5,16 @@ let fs = require('fs')
 
 var player = new mpg.MpgPlayer()
 
-let msg = 'abc'
+let msg = '现在是23:22'
 
 const stream = request(`https://ai.baidu.com/aidemo?type=tns2&idx=1&tex=${decodeURIComponent(msg)}&cuid=baidu_speech_demo&cod=2&lan=zh&ctp=1&pdt=1&spd=5&per=4&vol=9&pit=5`)
     .pipe(fs.createWriteStream('test.mp3'))
 
 stream.on('finish', () => {
+    console.log('play')
     player.play(`${__dirname}/test.mp3`)
+});
+
+stream.on('error', err => {
+    console.log(err)
 });
