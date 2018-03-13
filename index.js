087a75e4d79b5d138f9dbded51e3215df7f54461
CodeMonkey <ilovevar@gmail.com>
2018-03-13 18:06:31 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-let clock = require('./clock')
+let clock = require('./clock')()
 
 
 setInterval(() => {
 	clock.tick()							
 }, 1000)
+
+clock.baoshi('亲爱的，现在时间是' + (new Date()).toLocaleString())
