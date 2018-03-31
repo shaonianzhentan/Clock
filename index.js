66ed6157f98d54b9e3c81129c5c5bfa45c3575ae
shaonianzhentan <shaonianzhentan@msn.cn>
2018-03-31 12:00:13 +0800

    update

--- index.js
+++ index.js
@@ -24,10 +24,6 @@ gpio.setup(PIN, gpio.DIR_IN, err => {
 		console.log(err)
 		return
 	}
-	let obj = {
-		a: 0,
-		b: 0
-	}
 	setInterval(() => {
 		gpio.read(PIN, function (err, value) {
 			if (err) {
@@ -35,23 +31,9 @@ gpio.setup(PIN, gpio.DIR_IN, err => {
 				return
 			}
 			if (value) {
-				if (obj.b > 0) {
-					console.log('fail', obj.b)
-					obj.b = 0
-				}
-				obj.a += 1
-			} else {
-				if (obj.a > 0) {
-					console.log('success', obj.a)
-					if (obj.a > 10 && clock.music.musicList.length > 0) {
-						clock.baoshi(`亲爱的，现在时间是${moment().format('LLLL')}`)
-						console.log((new Date()).toLocaleString(), '有人')
-					}
-					obj.a = 0
-				}
-				obj.b += 1
+				clock.baoshi(`亲爱的，现在时间是${moment().format('LLLL')}`)
+				console.log((new Date()).toLocaleString(), '有人')
 			}
-			//console.log(value)
 		})
 	}, 1000)
 })
